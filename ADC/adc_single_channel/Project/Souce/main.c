11cc60ef7cdae9011cfa35f221bee4f9522158b1
xiaothinking <2681232282@qq.com>
2021-03-23 22:06:14 +0800

    ADC单通道采集

---
+++ ADC/adc_single_channel/Project/Souce/main.c
@@ -0,0 +1,65 @@
+/**
+  ******************************************************************************
+  * @file    Project/STM32F4xx_StdPeriph_Templates/main.c 
+  * @author  MCD Application Team
+  * @version V1.8.0
+  * @date    04-November-2016
+  * @brief   Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; COPYRIGHT 2016 STMicroelectronics</center></h2>
+  *
+  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
+  * You may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at:
+  *
+  *        http://www.st.com/software_license_agreement_liberty_v2
+  *
+  * Unless required by applicable law or agreed to in writing, software 
+  * distributed under the License is distributed on an "AS IS" BASIS, 
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "uart.h"
+#include "delay.h"
+#include "key.h"
+#include "adc.h"
+
+uint8_t g_key=255;
+uint16_t g_adc_value=0;
+float g_voltage=0;
+/**
+  * @brief  Main program
+  * @param  None
+  * @retval None
+  */
+int main(void)
+{
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);//设置系统中断优先级分组2
+  uart_init(9600);
+	delay_init(168);	 
+	key_init();
+  adc_init();
+  while (1)
+  {
+	   g_key=key_scan();
+		 if(g_key==0)
+     {
+			 g_key=255;
+       g_adc_value=get_adc_average(ADC_Channel_5,10);
+			 g_voltage=(g_adc_value*3.3)/4096;
+			 printf("g_adc_value=%d,g_voltage=%f",g_adc_value,g_voltage);
+		 }
+  }
+}
+
+
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
