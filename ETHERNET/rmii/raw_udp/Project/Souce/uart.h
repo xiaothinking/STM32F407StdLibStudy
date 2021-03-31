ebd03cdeda5c3ada01ce65a7f481bce837ca5563
xiaothinking <2681232282@qq.com>
2021-03-31 21:11:31 +0800

    UDP协议

---
+++ ETHERNET/rmii/raw_udp/Project/Souce/uart.h
@@ -0,0 +1,18 @@
+#ifndef _UART_H_
+#define _UART_H_
+
+#include "stm32f4xx.h"
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+#define USART_REC_LEN  			200  	//定义最大接收字节数 200
+#define EN_USART1_RX 			1		//使能（1）/禁止（0）串口1接收
+	  	
+extern u8  USART_RX_BUF[USART_REC_LEN]; //接收缓冲,最大USART_REC_LEN个字节.末字节为换行符 
+extern u16 USART_RX_STA;         		//接收状态标记	
+
+void uart_init(uint32_t bound);
+
+#endif
+
