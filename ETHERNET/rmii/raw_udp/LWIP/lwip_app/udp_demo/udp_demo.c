7b2b97215c0a4182015c57f4c2a71d6c3a1b4c8c
xiaothinking <2681232282@qq.com>
2021-11-14 17:50:47 +0800

    udp 通讯不带操作系统代码优化

--- ETHERNET/rmii/raw_udp/LWIP/lwip_app/udp_demo/udp_demo.c
+++ ETHERNET/rmii/raw_udp/LWIP/lwip_app/udp_demo/udp_demo.c
@@ -25,8 +25,8 @@
  
 //UDP接收数据缓冲区
 u8 udp_demo_recvbuf[UDP_DEMO_RX_BUFSIZE];	//UDP接收数据缓冲区 
-//UDP发送数据内容
-const u8 *tcp_demo_sendbuf="Explorer STM32F407 UDP demo send data\r\n";
+////UDP发送数据内容
+//const u8 *tcp_demo_sendbuf="Explorer STM32F407 UDP demo send data\r\n";
 
 //UDP 测试全局状态标记变量
 //bit7:没有用到
@@ -35,75 +35,15 @@ const u8 *tcp_demo_sendbuf="Explorer STM32F407 UDP demo send data\r\n";
 //bit4~0:保留
 u8 udp_demo_flag;
 
-//设置远端IP地址
-void udp_demo_set_remoteip(void)
-{
-	u8 *tbuf;
-	u16 xoff;
-	u8 key;
-	LCD_Clear(WHITE);
-	POINT_COLOR=RED;
-	LCD_ShowString(30,30,200,16,16,"Explorer STM32F4");
-	LCD_ShowString(30,50,200,16,16,"UDP Test");
-	LCD_ShowString(30,70,200,16,16,"Remote IP Set");  
-	LCD_ShowString(30,90,200,16,16,"KEY0:+  KEY2:-");  
-	LCD_ShowString(30,110,200,16,16,"KEY_UP:OK");  
-	tbuf=mymalloc(SRAMIN,100);	//申请内存
-	if(tbuf==NULL)return;
-	//前三个IP保持和DHCP得到的IP一致
-	lwipdev.remoteip[0]=lwipdev.ip[0];
-	lwipdev.remoteip[1]=lwipdev.ip[1];
-	lwipdev.remoteip[2]=lwipdev.ip[2]; 
-	sprintf((char*)tbuf,"Remote IP:%d.%d.%d.",lwipdev.remoteip[0],lwipdev.remoteip[1],lwipdev.remoteip[2]);//远端IP
-	LCD_ShowString(30,150,210,16,16,tbuf); 
-	POINT_COLOR=BLUE;
-	xoff=strlen((char*)tbuf)*8+30;
-	LCD_ShowxNum(xoff,150,lwipdev.remoteip[3],3,16,0); 
-	while(1)
-	{
-		key=key_scan(0);
-		if(key==4)break;
-		else if(key)
-		{
-			if(key==1)lwipdev.remoteip[3]++;//IP增加
-			if(key==3)lwipdev.remoteip[3]--;//IP减少
-			LCD_ShowxNum(xoff,150,lwipdev.remoteip[3],3,16,0X80);//显示新IP
-		}
-	}
-	myfree(SRAMIN,tbuf); 
-} 
-
+ 
 //UDP测试
+struct udp_pcb *udppcb;
 void udp_demo_test(void)
 {
  	err_t err;
-	struct udp_pcb *udppcb;  	//定义一个TCP服务器控制块
-	struct ip_addr rmtipaddr;  	//远端ip地址
+//	struct udp_pcb *udppcb;  	//定义一个TCP服务器控制块
+	struct ip_addr rmtipaddr;  	//远端ip地址	
  	
-	u8 *tbuf;
- 	u8 key;
-	u8 res=0;		
-	u8 t=0; 
- 	
-	udp_demo_set_remoteip();//先选择IP
-	LCD_Clear(WHITE);	//清屏
-	POINT_COLOR=RED; 	//红色字体
-	LCD_ShowString(30,30,200,16,16,"Explorer STM32F4");
-	LCD_ShowString(30,50,200,16,16,"UDP Test");
-	LCD_ShowString(30,70,200,16,16,"ATOM@ALIENTEK");  
-	LCD_ShowString(30,90,200,16,16,"KEY0:Send data");  
-	LCD_ShowString(30,110,200,16,16,"KEY_UP:Quit"); 
-	LCD_ShowString(30,130,200,16,16,"KEY1:Connect");
-	tbuf=mymalloc(SRAMIN,200);	//申请内存
-	if(tbuf==NULL)return ;		//内存申请失败了,直接退出
-	sprintf((char*)tbuf,"Local IP:%d.%d.%d.%d",lwipdev.ip[0],lwipdev.ip[1],lwipdev.ip[2],lwipdev.ip[3]);//服务器IP
-	LCD_ShowString(30,150,210,16,16,tbuf);  
-	sprintf((char*)tbuf,"Remote IP:%d.%d.%d.%d",lwipdev.remoteip[0],lwipdev.remoteip[1],lwipdev.remoteip[2],lwipdev.remoteip[3]);//远端IP
-	LCD_ShowString(30,170,210,16,16,tbuf);  
-	sprintf((char*)tbuf,"Remote Port:%d",UDP_DEMO_PORT);//客户端端口号
-	LCD_ShowString(30,190,210,16,16,tbuf);
-	POINT_COLOR=BLUE;
-	LCD_ShowString(30,210,210,16,16,"STATUS:Disconnected"); 
 	udppcb=udp_new();
 	if(udppcb)//创建成功
 	{ 
@@ -115,34 +55,11 @@ void udp_demo_test(void)
 			if(err==ERR_OK)	//绑定完成
 			{
 				udp_recv(udppcb,udp_demo_recv,NULL);//注册接收回调函数 
-				LCD_ShowString(30,210,210,16,16,"STATUS:Connected   ");//标记连接上了(UDP是非可靠连接,这里仅仅表示本地UDP已经准备好)
 				udp_demo_flag |= 1<<5;			//标记已经连接上
-				POINT_COLOR=RED;
-				LCD_ShowString(30,230,lcddev.width-30,lcddev.height-190,16,"Receive Data:");//提示消息		
-				POINT_COLOR=BLUE;//蓝色字体
-			}else res=1;
-		}else res=1;		
-	}else res=1;
-	while(res==0)
-	{
-		key=key_scan(0);
-		if(key==4)break;
-		if(key==1)//KEY0按下了,发送数据
-		{
-			udp_demo_senddata(udppcb);
-		}
-		if(udp_demo_flag&1<<6)//是否收到数据?
-		{
-			LCD_Fill(30,250,lcddev.width-1,lcddev.height-1,WHITE);//清上一次数据
-			LCD_ShowString(30,250,lcddev.width-30,lcddev.height-230,16,udp_demo_recvbuf);//显示接收到的数据			
-			udp_demo_flag&=~(1<<6);//标记数据已经被处理了.
-		} 
-		lwip_periodic_handle();
-		delay_ms(2);
-
+			}
+		}		
 	}
-	udp_demo_connection_close(udppcb); 
-	myfree(SRAMIN,tbuf);
+
 } 
 
 //UDP回调函数
@@ -173,15 +90,6 @@ void udp_demo_recv(void *arg,struct udp_pcb *upcb,struct pbuf *p,struct ip_addr 
 	}else
 	{
 		udp_disconnect(upcb); 
-		LCD_Clear(WHITE);			//清屏
-		POINT_COLOR = RED;
-		LCD_ShowString(30,30,200,16,16,"Explorer STM32F4");
-		LCD_ShowString(30,50,200,16,16,"UDP Test");
-		LCD_ShowString(30,70,200,16,16,"ATOM@ALIENTEK");
-		
-		POINT_COLOR=BLUE;
-		LCD_ShowString(30,90,200,16,16,"Connect break！");  
-		LCD_ShowString(30,110,200,16,16,"KEY1:Connect");
 		udp_demo_flag &= ~(1<<5);	//标记连接断开
 	} 
 } 
@@ -189,10 +97,10 @@ void udp_demo_recv(void *arg,struct udp_pcb *upcb,struct pbuf *p,struct ip_addr 
 void udp_demo_senddata(struct udp_pcb *upcb)
 {
 	struct pbuf *ptr;
-	ptr=pbuf_alloc(PBUF_TRANSPORT,strlen((char*)tcp_demo_sendbuf),PBUF_POOL); //申请内存
+	ptr=pbuf_alloc(PBUF_TRANSPORT,strlen((char*)udp_demo_recvbuf),PBUF_POOL); //申请内存
 	if(ptr)
 	{
-		pbuf_take(ptr,(char*)tcp_demo_sendbuf,strlen((char*)tcp_demo_sendbuf)); //将tcp_demo_sendbuf中的数据打包进pbuf结构中
+		pbuf_take(ptr,(char*)udp_demo_recvbuf,strlen((char*)udp_demo_recvbuf)); //将tcp_demo_sendbuf中的数据打包进pbuf结构中
 		udp_send(upcb,ptr);	//udp发送数据 
 		pbuf_free(ptr);//释放内存
 	} 
@@ -203,15 +111,6 @@ void udp_demo_connection_close(struct udp_pcb *upcb)
 	udp_disconnect(upcb); 
 	udp_remove(upcb);			//断开UDP连接 
 	udp_demo_flag &= ~(1<<5);	//标记连接断开
-	LCD_Clear(WHITE);			//清屏
-	POINT_COLOR = RED;
-	LCD_ShowString(30,30,200,16,16,"Explorer STM32F4");
-	LCD_ShowString(30,50,200,16,16,"UDP Test");
-	LCD_ShowString(30,70,200,16,16,"ATOM@ALIENTEK");
-	
-	POINT_COLOR=BLUE;
-	LCD_ShowString(30,90,200,16,16,"Connect break！");  
-	LCD_ShowString(30,110,200,16,16,"KEY1:Connect");
 }
 
 
